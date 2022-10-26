a9fd7e93ed14a155067b9e38a99e9e822cc93e37
MrKamikazee <m.roman1@uandresbello.edu>
2022-10-26 09:51:38 -0300

    Movimiento inicial
    El movimiento está casí listo, solo tengo que revisar en mi computadora unas lineas de codigo para pulirlo un poco más.

--- Assets/01_Scripts/PlayerMovemment.cs
+++ Assets/01_Scripts/PlayerMovemment.cs
@@ -1,8 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovemment : MonoBehaviour
 {
-    
+    private CharacterController Player_CC;
+    private Vector2 Speed = new Vector2(10,20);
+    private GameObject Piso;
+    private bool IsGrounded;
+    private Vector3 Direccion = new Vector3();
+
+    private void Awake()
+    {
+        Player_CC = GetComponent<CharacterController>();
+        Piso = GameObject.FindWithTag("Floor");
+    }
+
+    private void Update()
+    {
+        Direccion.x = Input.GetAxis("Horizontal") * Speed.x;
+        Direccion.z = Input.GetAxis("Vertical") * Speed.x;
+        
+        CheckGrounded();
+        
+        if (IsGrounded && Input.GetButton("Jump"))
+        {
+            Direccion.y = Speed.y;
+            IsGrounded = false;
+        }
+        else
+            Direccion.y -= .3f;
+
+        if (Direccion.y > 20)
+            Direccion.y = 20;
+        Player_CC.Move(Direccion * Time.deltaTime);
+    }
+
+    private void CheckGrounded()
+    {
+        RaycastHit hit;
+        
+        Debug.DrawRay(transform.localPosition, Vector3.down * 1.2f, Color.red);
+
+        if (Physics.Raycast(transform.localPosition, Vector3.down, out hit, 1.2f))
+        {
+            IsGrounded = Piso.CompareTag("Floor") ? true : false;
+        }
+    }
 }
